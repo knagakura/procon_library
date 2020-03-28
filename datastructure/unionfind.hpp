8c5ef895ce812eea206ccdeaa84f59a993acc2a7
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-03-28 17:29:09 +0900

    add macros and test using unionfind

---
+++ datastructure/unionfind.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "../macros.hpp"
+/*
+@title UnionFind Tree
+@category datastructure
+@docs ../docs/datastructure/unionfind.md
+*/
+struct UnionFind{
+    int n;
+    vector<int> Parent;
+    vector<int> sizes;
+    UnionFind(int _n):n(_n),Parent(_n),sizes(_n,1){ rep(i,n)Parent[i]=i; }
+    //find the root of x
+    int root(int x){
+        if(x!=Parent[x]){
+        Parent[x] = root(Parent[x]);
+        }
+        return Parent[x];
+    }
+    //merge x and y
+    void unite(int x,int y){
+        x = root(x);
+        y = root(y);
+        if(x == y) return;
+        if(sizes[x] < sizes[y]) swap(x, y);
+        Parent[y] = x;
+        sizes[x] += sizes[y];
+    }
+    bool same(int x,int y){ return root(x) == root(y); }
+    int size(int x){ return sizes[root(x)]; }
+    int group_num(){
+        set<int> s;
+        for(int i = 0; i < n; ++i){
+            s.insert(root(i));
+        }
+        return int(s.size());
+    }
+};
---
+++ test/DSL_1_A.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=jp"
+#include "../macros.hpp"
+#include "../datastructure/unionfind.hpp"
+
+int main(){
+    int N, Q;
+    cin >> N >> Q;
+    UnionFind Tree(N);
+    while(Q--){
+        int com, x, y;
+        cin >> com >> x >> y;
+        if(com == 0){
+            Tree.unite(x, y);
+        }
+        if(com == 1){
+            cout << Tree.same(x, y) << endl;
+        }
+    }
+}
