180452a306884832bc865d93da0ede6fc4bcf004
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-03-30 14:21:06 +0900

    0 or 1

--- test/DPL_5_K.test.cpp
+++ test/DPL_5_K.test.cpp
@@ -1,10 +1,10 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_H"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_K"
 #include "../macro/macros.hpp"
 #include "../math/mint.hpp"
 #include "../math/comb.hpp"
 
 /*
-@title 玉区別する、箱区別しない、1個以内(0 or 1)
+@title 玉区別しない、箱区別しない、1個以内(0 or 1)
 @category 写像12相
 */
 int main(){
