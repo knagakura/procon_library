cce331b7689011b1a4e86d248592340c907767d0
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-05-09 16:39:18 +0900

    geometory

---
+++ geometory/template.cpp
@@ -0,0 +1,37 @@
+#ifndef GEOMETORY_TEMPLATE_CPP
+#define GEOMETORY_TEMPLATE_CPP
+#include "../macro/macros.hpp"
+#include <complex>
+/*
+@title Geometory/template.cpp
+@category Geometory
+*/
+
+typedef complex<double> Point;
+typedef pair<Point, Point> Segment;
+#define EPS (1e-10)
+#define EQ(a, b) (abs((a) - (b)) < EPS)
+
+// 内積
+double dot(Point a, Point b){
+    return (a.real() * b.real() + a.imag() * b.imag());
+}
+//外積
+double cross(const Point &a, const Point &b){
+    return (a.real() * b.imag() - a.imag() * b.real());
+}
+
+/*
+"https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_1_A"
+直線 l に p から引いた交点を求める
+*/
+Point projection(const Segment &l, const Point &p){
+    double t = dot(p - l.first, l.second - l.first) / norm(l.second - l.first);
+    return l.first + t * (l.second - l.first);
+}
+
+bool is_intersected_ls(Point a1, Point a2, Point b1, Point b2){
+    return (cross(a2-a1, b1-a1)*cross(a2-a1, b2-b1) < EPS) && (cross(b2-b1, a1-b1)*cross(b2-b1, a2-b1) < EPS);
+}
+#endif
+
---
+++ test/CGL_1_A.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_1_A"
+#include "../macro/macros.hpp"
+#include "../geometory/template.cpp"
+
+int main(){
+    cout << fixed << setprecision(20);
+    int xp1,yp1,xp2,yp2;
+    cin >> xp1 >> yp1 >> xp2 >> yp2;
+    Point p1(xp1,yp1);
+    Point p2(xp2,yp2);
+    Segment l = make_pair(p1, p2);
+    int q;
+    cin >> q;
+    rep(_,q){
+        int px, py;
+        cin >> px >> py;
+        Point p(px, py);
+        Point ans = projection(l, p);
+        cout << ans.real() << " " << ans.imag() << endl;
+    }
+}
