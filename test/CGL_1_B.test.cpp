0b8a7b5823214eb6858f96650094cbe791695065
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-05-09 18:06:04 +0900

    add 誤差

--- test/CGL_1_B.test.cpp
+++ test/CGL_1_B.test.cpp
@@ -1,21 +1,22 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_1_A"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_1_B"
 #include "../macro/macros.hpp"
 #include "../geometory/template.cpp"
+
 #define ERROR "1e-8"
 int main(){
     cout << fixed << setprecision(20);
-    int xp1,yp1,xp2,yp2;
+    int xp1, yp1, xp2, yp2;
     cin >> xp1 >> yp1 >> xp2 >> yp2;
-    Point p1(xp1,yp1);
-    Point p2(xp2,yp2);
+    Point p1(xp1, yp1);
+    Point p2(xp2, yp2);
     Segment l = make_pair(p1, p2);
     int q;
     cin >> q;
-    rep(_,q){
+    rep(_, q){
         int px, py;
         cin >> px >> py;
         Point p(px, py);
-        Point ans = projection(l, p);
+        Point ans = reflection(l, p);
         cout << ans.real() << " " << ans.imag() << endl;
     }
 }
