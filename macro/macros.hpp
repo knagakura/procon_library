e418313f4ce1aff4921b9af7d2bb2b8f9e8a403e
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-09-23 14:39:14 +0900

    update macros

--- macro/macros.hpp
+++ macro/macros.hpp
@@ -7,38 +7,51 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 #define rep(i,N) for(int i=0;i<int(N);++i)
 #define rep1(i,N) for(int i=1;i<int(N);++i)
 #define all(a) (a).begin(),(a).end()
-#define print(v) { cerr<<#v<<": [ "; for(auto _ : v) cerr<<_<<", "; cerr<<"]"<<endl; }
-#define printpair(v) { cerr<<#v<<": [ "; for(auto _ : v) cerr<<"{"<<_.first<<","<<_.second<<"}"<<", "; cerr<<"]"<<endl; }
-#define dump(x) cerr<<#x<<": "<<x<<endl;
 #define bit(k) (1LL<<(k))
-#define Yes "Yes"
-#define No "No"
-#define YES "YES"
-#define NO "NO"
-typedef long long ll;
+#define SUM(v) accumulate(all(v), 0LL)
 
-template< typename T1, typename T2 >
-ostream &operator<<(ostream &os, const pair< T1, T2 >& p) {
-  os << "{" <<p.first << ", " << p.second << "}";
-  return os;
-}
+typedef pair<int, int> i_i;
+typedef pair<ll, ll> l_l;
 template <class T> using vec = vector<T>;
 template <class T> using vvec = vector<vec<T>>;
+struct fast_ios{ fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }fast_ios_;
 
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
+#define TOSTRING(x) string(#x)
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (T &x : vec) is >> x; return is; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { os  << "["; for(auto _: v) os << _ << ", "; os << "]"; return os; };
+template <typename T> ostream &operator<<(ostream &os, set<T> &st) { os << "("; for(auto _: st) { os << _ << ", "; } os << ")";return os;}
+template <typename T, typename U> ostream &operator<<(ostream &os, const pair< T, U >& p){os << "{" <<p.first << ", " << p.second << "}";return os; }
+template <typename T, typename U> ostream &operator<<(ostream &os, const map<T, U> &mp){ os << "["; for(auto _: mp){ os << _ << ", "; } os << "]" << endl; return os; }
+
+#define DUMPOUT cerr
+void dump_func(){ DUMPOUT << endl; }
+template <class Head, class... Tail> void dump_func(Head &&head, Tail &&... tail) { DUMPOUT << head; if (sizeof...(Tail) > 0) { DUMPOUT << ", "; } dump_func(std::move(tail)...); }
+
+#ifdef DEBUG
+#define dbg(...) dump_func(__VA_ARGS__)
+#define dump(...) DUMPOUT << string(#__VA_ARGS__) << ": "; dump_func(__VA_ARGS__)
+#else
+#define dbg(...)
+#define dump(...)
+#endif
+
 const int INF = (ll)1e9;
 const ll INFLL = (ll)1e18+1;
-const ll MOD = (ll)1e9+7;
+const ll MOD = 1000000007;
+// const ll MOD = 998244353;
+const long double PI = acos(-1.0);
 
-const double PI = acos(-1.0);
-
+/*
 const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
 const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
 const string dir = "DRUL";
+*/
 
 #endif
